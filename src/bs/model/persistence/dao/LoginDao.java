053683870653729827c942a05ecd285d4f535d90
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-08-08 21:33:48 +0200

    Create DAO class for Login entity.

---
+++ src/bs/model/persistence/dao/LoginDao.java
@@ -0,0 +1,53 @@
+package bs.model.persistence.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import bs.model.persistence.entities.LoginEntity;
+
+public class LoginDao extends AbstractDao<LoginEntity> implements Dao<LoginEntity> {
+
+	private LoginValidator validator;
+	
+	public LoginDao() {
+		validator = new LoginValidator();
+	}
+	
+	@Override
+	public boolean create(LoginEntity login) {
+		if (!validator.validateBeforeCreate(login)) return false;
+		super.persistEntity(login);
+		return true;
+	}
+
+	@Override
+	public boolean update(LoginEntity login) {
+		if (!validator.validateBeforeUpdate(login)) return false;
+		super.mergeEntity(login);
+		return true;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		if (!validator.validateBeforeDelete(id)) return false;
+		super.removeEntity(LoginEntity.class, id);
+		return true;
+	}
+
+	@Override
+	public LoginEntity getById(int id) {
+		EntityManager entityManager = getPersistenceSupport().getEntityManager();
+		LoginEntity login = entityManager.find(LoginEntity.class, id);
+		return login;
+	}
+
+	@Override
+	public List<LoginEntity> getAll() {
+		EntityManager entityManager = getPersistenceSupport().getEntityManager();
+		List<LoginEntity> loginList = entityManager
+			.createQuery("FROM Login log", LoginEntity.class).getResultList();
+		return loginList;
+	}
+	
+}
