8c15a86681fad1597c34b3046d491adbb6f30fea
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-08-30 22:08:41 +0200

    Fix security classes code.

--- src/bs/model/services/security/IpGetter.java
+++ src/bs/model/services/security/IpGetter.java
@@ -3,18 +3,19 @@ package bs.model.services.security;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import javax.persistence.NoResultException;
+
 public class IpGetter {
 	
 	public static String getIp() {
-        String ipAddress = "";
-		InetAddress inetAddress = null;
 		try {
-			inetAddress = InetAddress.getLocalHost();
-			ipAddress = inetAddress.getHostAddress().toString();
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String ipAddress = inetAddress.getHostAddress().toString();
+			return ipAddress;
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
+			throw new NoResultException();
 		}
-		return ipAddress;
 	}
 	
 }
