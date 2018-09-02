3455d95de482d574c0dd025584a9fc137b8921cf
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-09-02 22:52:00 +0200

    Connect JSON Web Token library.

---
+++ src/bs/model/services/security/TokenBuilder.java
@@ -0,0 +1,30 @@
+package bs.model.services.security;
+
+import java.time.Instant;
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
+
+public class TokenBuilder {
+
+	public static String build(Integer accountNumber) {
+		return Jwts.builder()
+			.setIssuer(accountNumber.toString())
+			.setSubject("Subject")
+			// .claim("", "")
+			.setIssuedAt(Date.from(Instant.ofEpochSecond(new Date().getTime())))
+			.setExpiration(Date.from(Instant.ofEpochSecond(new Date().getTime())))
+			.signWith(
+				SignatureAlgorithm.HS256,
+				TextCodec.BASE64.decode("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=")
+			)
+			.compact();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
