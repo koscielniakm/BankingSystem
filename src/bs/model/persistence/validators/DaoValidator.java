6c94284356e1295d1d03ba3705ade7bffce9ed70
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-09-06 23:31:51 +0200

    Implement methods in validator interface.

--- src/bs/model/persistence/validators/DaoValidator.java
+++ src/bs/model/persistence/validators/DaoValidator.java
@@ -1,3 +1,7 @@
 package bs.model.persistence.validators;
 
-public interface DaoValidator<T> { }
+public interface DaoValidator<T> {
+	boolean validateBeforeCreate(T entity);
+	boolean validateBeforeUpdate(T entity);
+	boolean validateBeforeDelete(Integer id);
+}
