1fdb9d5db62654ce9868d0cdb7da4d34ff0bf0a2
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-09-06 23:05:39 +0200

    Refactor AccountValidator.

--- src/bs/model/persistence/validators/AccountValidator.java
+++ src/bs/model/persistence/validators/AccountValidator.java
@@ -1,58 +1,68 @@
 package bs.model.persistence.validators;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import bs.model.config.Finals;
 import bs.model.persistence.entities.AccountEntity;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class AccountValidator implements DaoValidator<AccountEntity> {
+public class AccountValidator extends AbstractValidator implements DaoValidator<AccountEntity> {
 
 	public boolean validateBeforeCreate(AccountEntity account) {
-		if (account == null) return false;
-		if (validateAccountNumber(account.getAccountNumber()) &&
-			validatePassword(account.getPassword()) &&
-			validateEmail(account.getEmail()))
-			return true;
-		else return false;
-	}
-
-	public boolean validateBeforeUpdate(AccountEntity account) {
-		if (account == null) return false;
-		if (account.getId() > 0 &&
-			validateAccountNumber(account.getAccountNumber()) &&
-			validatePassword(account.getPassword()) &&
-			validateEmail(account.getEmail()))
-			return true;
-		else return false;
-	}
-
-	public boolean validateBeforeDelete(Integer id) {
-		if (id > 0) return true;
-		return false;
-	}
-	
-	private boolean validateAccountNumber(Integer accountNumber) {
-		if (accountNumber == null)
-			return false;
-		if (accountNumber <= 0)
-			return false;
-		if (accountNumber.toString().length() < Finals.DB_ACCOUNT_ACCOUNTNUMBER_LENGTH)
-			return false;
+		for (Boolean validation : getValidationsBeforeCreate(account))
+			if (!validation) return false;
 		return true;
 	}
 	
-	private boolean validatePassword(String password) {
-		if (password == null || password.length() < Finals.ACCOUNT_PASSWORD_MIN_LENGTH)
-			return false;
-		if (password.length() > Finals.DB_ACCOUNT_PASSWORD_MAX_LENGTH)
-			return false;
-		else return true;
+	private List<Boolean> getValidationsBeforeCreate(AccountEntity account) {
+		List<Boolean> validationList = new ArrayList<>();
+		validationList.add(validateNotNull(account.getAccountNumber()));
+		validationList.add(validateNotNull(account.getPassword()));
+		validationList.add(validateNotNull(account.getEmail()));
+		validationList.add(validateNotNull(account.getOpenDate()));
+		validationList.add(validateAccountNumberLength(account.getAccountNumber()));
+		validationList.add(validateAccountNumberValue(account.getAccountNumber()));
+		validationList.add(validateEmailLength(account.getEmail()));
+		validationList.add(validateEmailSyntax(account.getEmail()));
+		return validationList;
 	}
 	
-	private boolean validateEmail(String email) {
-		if (email == null || email.length() > Finals.DB_ACCOUNT_EMAIL_LENGTH)
+	public boolean validateBeforeUpdate(AccountEntity entity) {
+		// TODO AccountValidator : validateBeforeUpdate(AccountEntity) impl.
+		throw new NotImplementedException();
+	}
+	
+	public boolean validateBeforeDelete(Integer id) {
+		// TODO AccountValidator : validateBeforeDelete(AccountEntity) impl.
+		throw new NotImplementedException();
+	}
+	
+	private boolean validateId(Integer id) {
+		return id == null || id < 0 ? false : true;
+	}
+	
+	private boolean validateAccountNumberValue(Integer accountNumber) {
+		Integer minValue = Finals.DB_ACCOUNT_ACCOUNTNUMBER_MIN_VALUE;
+		Integer maxValue = Finals.DB_ACCOUNT_ACCOUNTNUMBER_MAX_VALUE;
+		return accountNumber < minValue && accountNumber > maxValue ? true : false;
+	}
+	
+	private boolean validateAccountNumberLength(Integer accountNumber) {
+		if (accountNumber.toString().length() == Finals.DB_ACCOUNT_ACCOUNTNUMBER_LENGTH)
+			return true;
+		else
 			return false;
+	}
+	
+	private boolean validateEmailLength(String email) {
+		Integer maxLength = Finals.DB_ACCOUNT_EMAIL_LENGTH;
+		return email.length() > maxLength ? false : true;
+	}
+	
+	private boolean validateEmailSyntax(String email) {
 		Pattern emailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 		Matcher matcher = emailRegex.matcher(email);
 		return matcher.find();
