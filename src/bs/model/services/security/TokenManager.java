57944bd074467fe481d9056ca8cf4ede3376c388
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-09-06 21:45:57 +0200

    Update TokenManager.

--- src/bs/model/services/security/TokenManager.java
+++ src/bs/model/services/security/TokenManager.java
@@ -1,33 +1,37 @@
 package bs.model.services.security;
 
+
+import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.TextCodec;
 
-public class TokenManager {
+public final class TokenManager {
 
-	private static final String TOKEN_SECRET = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E";
+	private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
 	
+	private static final byte[] TOKEN_SECRET = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=".getBytes();
+	
+	private static final Key TOKEN_KEY = new SecretKeySpec(TOKEN_SECRET, SIGNATURE_ALGORITHM.getJcaName());
+
 	public static String build(Integer accountNumber) {
 		return Jwts.builder()
 			.setIssuer(accountNumber.toString())
 			.setSubject("Subject")
 			.setIssuedAt(Date.from(Instant.ofEpochSecond(new Date().getTime())))
 			.setExpiration(Date.from(Instant.ofEpochSecond(new Date().getTime())))
-			.signWith(
-				SignatureAlgorithm.HS256,
-				TextCodec.BASE64.decode(TOKEN_SECRET)
-			)
+			.signWith(SIGNATURE_ALGORITHM, TOKEN_KEY)
 			.compact();
 	}
 	
 	private static Claims decode(String token) {
 		return Jwts.parser()
-			.setSigningKey(TextCodec.BASE64.decode(TOKEN_SECRET))
+			.setSigningKey(TOKEN_SECRET)
 			.parseClaimsJws(token).getBody();
 	}
 	
