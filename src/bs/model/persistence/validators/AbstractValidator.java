6c012847e196071b731193e9f0e92ce37a961c1e
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-09-06 23:20:37 +0200

    Add AbstractValidator documentation.

--- src/bs/model/persistence/validators/AbstractValidator.java
+++ src/bs/model/persistence/validators/AbstractValidator.java
@@ -1,11 +1,25 @@
 package bs.model.persistence.validators;
 
+/**
+ * Superclass for DAO validators.
+ * @author Mateusz
+ */
 public abstract class AbstractValidator {
 
+	/**
+	 * Check if object is not null.
+	 * @param object Some object.
+	 * @return Result of validation.
+	 */
 	protected static boolean validateNotNull(Object object) {
 		return object == null ? false : true;
 	}
 	
+	/**
+	 * Check for correct ID.
+	 * @param id ID of entity.
+	 * @return Result of validation.
+	 */
 	protected static boolean validateId(Integer id) {
 		return id == null || id < 0 ? false : true;
 	}
