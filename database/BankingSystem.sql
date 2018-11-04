a88e3da90010119766729bca99596793084b40fb
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-11-04 11:09:40 +0100

    Create new database template.

---
+++ database/BankingSystem.sql
@@ -0,0 +1,124 @@
+CREATE TABLE `Client` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Client_Number` bigint(9) NOT NULL,
+	`Password` varchar(64) NOT NULL,
+	`Email` varchar(50),
+	`Date_Open` DATETIME NOT NULL,
+	`Date_Close` DATETIME,
+	`First_Name` varchar(30),
+	`Last_Name` varchar(30),
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Account` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Client_ID` bigint NOT NULL,
+	`Account_Type_ID` int NOT NULL,
+	`Amount` double NOT NULL,
+	`Account_Number` bigint(26) NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Account_Type` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Type` varchar(20) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Authorizations` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Client_ID` bigint NOT NULL,
+	`IP_Address` varchar(15) NOT NULL,
+	`Result` bit(1) NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Money_Transfer_Data` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Sender_Account_Number` bigint NOT NULL,
+	`Sender_Name` bigint NOT NULL,
+	`Recipient_Account_Number` bigint NOT NULL,
+	`Recipient_Name` bigint NOT NULL,
+	`Title` varchar(50) NOT NULL,
+	`Amount` double NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Card` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Account_ID` bigint(9) NOT NULL,
+	`Card_Type_ID` bigint NOT NULL,
+	`Card_Number` int(9) NOT NULL,
+	`Date_Active` DATETIME NOT NULL,
+	`Date_Unactive` DATETIME NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Payment` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Card_ID` bigint NOT NULL,
+	`Country` varchar(20) NOT NULL,
+	`City` varchar(30) NOT NULL,
+	`Address` varchar(40) NOT NULL,
+	`Date` DATETIME NOT NULL,
+	`Amount` double NOT NULL,
+	`Title` varchar(30) NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Card_Type` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Type` varchar(15) NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Cyclical_Money_Transfer` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Account_ID` bigint NOT NULL,
+	`Money_Transfer_Data_ID` bigint NOT NULL,
+	`Date_Active` DATETIME NOT NULL,
+	`Date_Delay` DATETIME NOT NULL,
+	`Date_Unactive` DATETIME NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Incoming_Money_Transfer` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Account_ID` bigint NOT NULL,
+	`Money_Transfer_Data_ID` bigint NOT NULL,
+	`Date_Receive` DATETIME NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `Outgoing_Money_Transfer` (
+	`ID` bigint NOT NULL AUTO_INCREMENT,
+	`Account_ID` bigint NOT NULL,
+	`Money_Transfer_Data_ID` bigint NOT NULL,
+	`Date_Send` DATETIME NOT NULL,
+	PRIMARY KEY (`ID`)
+);
+
+ALTER TABLE `Account` ADD CONSTRAINT `Account_fk0` FOREIGN KEY (`Client_ID`) REFERENCES `Client`(`ID`);
+
+ALTER TABLE `Account` ADD CONSTRAINT `Account_fk1` FOREIGN KEY (`Account_Type_ID`) REFERENCES `Account_Type`(`ID`);
+
+ALTER TABLE `Authorizations` ADD CONSTRAINT `Authorizations_fk0` FOREIGN KEY (`Client_ID`) REFERENCES `Client`(`ID`);
+
+ALTER TABLE `Card` ADD CONSTRAINT `Card_fk0` FOREIGN KEY (`Account_ID`) REFERENCES `Account`(`ID`);
+
+ALTER TABLE `Card` ADD CONSTRAINT `Card_fk1` FOREIGN KEY (`Card_Type_ID`) REFERENCES `Card_Type`(`ID`);
+
+ALTER TABLE `Payment` ADD CONSTRAINT `Payment_fk0` FOREIGN KEY (`Card_ID`) REFERENCES `Card`(`ID`);
+
+ALTER TABLE `Cyclical_Money_Transfer` ADD CONSTRAINT `Cyclical_Money_Transfer_fk0` FOREIGN KEY (`Account_ID`) REFERENCES `Account`(`ID`);
+
+ALTER TABLE `Cyclical_Money_Transfer` ADD CONSTRAINT `Cyclical_Money_Transfer_fk1` FOREIGN KEY (`Money_Transfer_Data_ID`) REFERENCES `Money_Transfer_Data`(`ID`);
+
+ALTER TABLE `Incoming_Money_Transfer` ADD CONSTRAINT `Incoming_Money_Transfer_fk0` FOREIGN KEY (`Account_ID`) REFERENCES `Account`(`ID`);
+
+ALTER TABLE `Incoming_Money_Transfer` ADD CONSTRAINT `Incoming_Money_Transfer_fk1` FOREIGN KEY (`Money_Transfer_Data_ID`) REFERENCES `Money_Transfer_Data`(`ID`);
+
+ALTER TABLE `Outgoing_Money_Transfer` ADD CONSTRAINT `Outgoing_Money_Transfer_fk0` FOREIGN KEY (`Account_ID`) REFERENCES `Account`(`ID`);
+
+ALTER TABLE `Outgoing_Money_Transfer` ADD CONSTRAINT `Outgoing_Money_Transfer_fk1` FOREIGN KEY (`Money_Transfer_Data_ID`) REFERENCES `Money_Transfer_Data`(`ID`);
+
